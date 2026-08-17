94c8b96d9ea89cb3fe1d1d29ef2635dcba9b5651
FabioAzzini <75931076+FabioAzzini@users.noreply.github.com>
2026-08-17 13:52:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gestione-interventi-v1';
+const CACHE='gestione-interventi-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
